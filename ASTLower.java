7ae44379dfef3a51e8a465014f31d532c9017a62
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-07 15:36:54 +0100

    feat(ASTLower): implement ASTLower class for less-than evaluation of nodes

---
+++ ASTLower.java
@@ -0,0 +1,18 @@
+public class ASTLower implements ASTNode {
+    ASTNode lhs, rhs;
+
+    public IValue eval(Environment<IValue> e) throws InterpreterError {
+        IValue v1 = lhs.eval(e);
+        IValue v2 = rhs.eval(e);
+        
+        if (v1 instanceof VInt && v2 instanceof VInt) {
+            return new VBool(((VInt)v1).getval() < ((VInt)v2).getval());
+        }
+        throw new InterpreterError("Type error: < requires numeric operands");
+    }
+
+    public ASTLower(ASTNode l, ASTNode r) {
+        lhs = l;
+        rhs = r;
+    }
+}
