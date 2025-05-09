99e0185a96b81b5d8b46f98ba91c07607b251404
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-09 17:03:37 +0100

    feat(ASTMatch): implement ASTMatch class for pattern matching on lists

---
+++ ASTMatch.java
@@ -0,0 +1,43 @@
+public class ASTMatch implements ASTNode {
+    private ASTNode expr;
+    private ASTNode nilCase;
+    private String headVar;
+    private String tailVar;
+    private ASTNode consCase;
+    
+    public ASTMatch(ASTNode expr, ASTNode nilCase, String headVar, String tailVar, ASTNode consCase) {
+        this.expr = expr;
+        this.nilCase = nilCase;
+        this.headVar = headVar;
+        this.tailVar = tailVar;
+        this.consCase = consCase;
+    }
+    
+    public IValue eval(Environment<IValue> e) throws InterpreterError {
+        IValue value = expr.eval(e);
+        
+        if (value instanceof VList) {
+            VList list = (VList) value;
+            if (list.isNil()) {
+                return nilCase.eval(e);
+            } else {
+                Environment<IValue> newEnv = e.beginScope();
+                newEnv.assoc(headVar, list.getHead());
+                newEnv.assoc(tailVar, list.getTail());
+                return consCase.eval(newEnv);
+            }
+        } else if (value instanceof VLazyList) {
+            VLazyList lazyList = (VLazyList) value;
+            if (lazyList.isNil()) {
+                return nilCase.eval(e);
+            } else {
+                Environment<IValue> newEnv = e.beginScope();
+                newEnv.assoc(headVar, lazyList.getHead());
+                newEnv.assoc(tailVar, lazyList.getTail());
+                return consCase.eval(newEnv);
+            }
+        } else {
+            throw new InterpreterError("Match expression must evaluate to a list");
+        }
+    }
+}
