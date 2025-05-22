1d39524f5890c2a0ac1020d3658bb47382f7a159
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-22 17:11:33 +0100

    refactor(ASTInt): clarify variable access in constructor and eval method by using 'this' keyword

--- ASTInt.java
+++ ASTInt.java
@@ -1,13 +1,12 @@
 class ASTInt implements ASTNode  {
     private final int v;
 
-    ASTInt(int v0) {
-        this.v = v0;
+    ASTInt(int v) {
+        this.v = v;
     }
 
     @Override
     public IValue eval(Environment<IValue> e) throws InterpreterError {
-        return new VInt(v);                
+        return new VInt(this.v);
     }
-
 }
