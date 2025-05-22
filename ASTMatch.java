d26b2d128d8b74d11ed511be7b7de6918c045fdc
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-22 17:21:28 +0100

    refactor(ASTMatch): improve comments and clarify variable access in eval method using 'this' keyword

--- ASTMatch.java
+++ ASTMatch.java
@@ -17,42 +17,42 @@ public class ASTMatch implements ASTNode {
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         final IValue value = this.expr.eval(e);
         
-        // Case 1: Regular list
+        // Case 1: Regular strict list
         switch (value) {
             case VList vList -> {
-                if (vList.isNil()) {
-                    return nilCase.eval(e);
-                } else {
+                if (vList.isNil())
+                    return this.nilCase.eval(e);
+                else {
                     final Environment<IValue> newEnv = e.beginScope();
-                    newEnv.assoc(headVar, vList.getHead());
-                    newEnv.assoc(tailVar, vList.getTail());
-                    return consCase.eval(newEnv);
+
+                    newEnv.assoc(this.headVar, vList.getHead());
+                    newEnv.assoc(this.tailVar, vList.getTail());
+
+                    return this.consCase.eval(newEnv);
                 }
             }
             case VLazyList lazyList -> {
-                // Force evaluation of the lazy list when matched
+                // It is here that we force the evaluation of the lazy list when matched
                 try {
-                    // Get head and tail - this will force evaluation if needed
-                    IValue head = lazyList.getHead();
-                    IValue tail = lazyList.getTail();
+                    final IValue head = lazyList.getHead();
+                    final IValue tail = lazyList.getTail();
                     
-                    // Create new environment with bindings for head and tail
                     final Environment<IValue> newEnv = e.beginScope();
-                    newEnv.assoc(headVar, head);
-                    newEnv.assoc(tailVar, tail);
+                    newEnv.assoc(this.headVar, head);
+                    newEnv.assoc(this.tailVar, tail);
                     
-                    return consCase.eval(newEnv);
-                } catch (InterpreterError ex) {
+                    return this.consCase.eval(newEnv);
+                } catch (InterpreterError err) {
                     // If there's an error evaluating the lazy list, it might be nil
+                    // Although this is rarely the case
                     try {
-                        if (lazyList.isNil()) {
-                            return nilCase.eval(e);
-                        }
-                    } catch (InterpreterError inner) {
-                        // Re-throw the original error
-                        throw ex;
+                        if (lazyList.isNil())
+                            return this.nilCase.eval(e);
+                    } catch (InterpreterError err2) {
+                        throw err;
                     }
-                    throw ex;
+                    
+                    throw err;
                 }
             }
             default -> throw new InterpreterError("Match expression must evaluate to a list or lazy list");
