c2421abf1cccc879edd9ffe0c037d7ed33ca539c
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-22 17:27:49 +0100

    refactor(ASTSub): clarify variable access in eval method by using 'this' keyword

--- ASTSub.java
+++ ASTSub.java
@@ -9,12 +9,12 @@ public class ASTSub implements ASTNode {
 
 	@Override
     public IValue eval(Environment<IValue> e) throws InterpreterError {
-		final IValue v1 = lhs.eval(e);
-		final IValue v2 = rhs.eval(e);
-		if (v1 instanceof VInt && v2 instanceof VInt) {
-			return new VInt(((VInt) v1).getVal() - ((VInt) v2).getVal());
-		} else {
-			throw new InterpreterError("illegal types to + operator");
-		}
+		final IValue v1 = this.lhs.eval(e);
+		final IValue v2 = this.rhs.eval(e);
+
+		if (v1 instanceof VInt val1 && v2 instanceof VInt val2)
+			return new VInt(val1.getVal() - val2.getVal());
+		else
+			throw new InterpreterError("illegal types to - (subtract) operator");
     }
 }
