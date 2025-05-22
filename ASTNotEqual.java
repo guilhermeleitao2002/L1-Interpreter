91b231e83de10a074a51038f57b7158f2c224f59
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-05-22 16:32:13 +0100

    refactor(ASTEqual, ASTNotEqual): clarify variable access in eval method by using 'this' keyword

--- ASTNotEqual.java
+++ ASTNotEqual.java
@@ -9,15 +9,15 @@ public class ASTNotEqual implements ASTNode {
 
     @Override
     public IValue eval(Environment<IValue> e) throws InterpreterError {
-        final IValue v1 = lhs.eval(e);
-        final IValue v2 = rhs.eval(e);
+        final IValue v1 = this.lhs.eval(e);
+        final IValue v2 = this.rhs.eval(e);
         
-        if (v1 instanceof VInt && v2 instanceof VInt) {
+        if (v1 instanceof VInt && v2 instanceof VInt)
             return new VBool(((VInt)v1).getVal() != ((VInt)v2).getVal());
-        }
-        if (v1 instanceof VBool && v2 instanceof VBool) {
+
+        if (v1 instanceof VBool && v2 instanceof VBool)
             return new VBool(((VBool)v1).getValue() != ((VBool)v2).getValue());
-        }
-        throw new InterpreterError("Type error: ~= requires matching numeric or boolean operands");
+
+        throw new InterpreterError("~= requires matching numeric or boolean operands");
     }
 }
