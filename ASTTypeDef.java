357524b99e8c96d1e47fec3d9bf34d60666ce524
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 23:06:58 +0100

    feat(ASTTypeDef): refactor constructor and update typecheck method

--- ASTTypeDef.java
+++ ASTTypeDef.java
@@ -1,16 +1,28 @@
 import java.util.*;
 
 public class ASTTypeDef implements ASTNode {
-    HashMap<String,ASTType> ltd;
-    ASTNode body;
+    private final Map<String, ASTType> typeDefs;
+    private final ASTNode body;
 
-    public ASTTypeDef(HashMap<String,ASTType> ltdp, ASTNode b) {
-        this.ltd = ltdp;
-        this.body = b;
+    public ASTTypeDef(Map<String, ASTType> typeDefs, ASTNode body) {
+        this.typeDefs = typeDefs;
+        this.body = body;
     }
     
     @Override
     public IValue eval(Environment<IValue> env) throws InterpreterError {
         return this.body.eval(env);
+    }
+    
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefEnv) throws TypeError {
+        final TypeDefEnvironment newTypeDefEnv = typeDefEnv.beginScope();
+        
+        // Add all type definitions to the environment
+        for (Map.Entry<String, ASTType> entry : this.typeDefs.entrySet()) {
+            newTypeDefEnv.assoc(entry.getKey(), entry.getValue());
+        }
+        
+        return this.body.typecheck(gamma, newTypeDefEnv);
     }
 }
