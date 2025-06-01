929da08342c909c5f894b88844ad3f01b5644498
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 23:05:20 +0100

    feat(ASTNil): implement typecheck method for ASTNil class

--- ASTNil.java
+++ ASTNil.java
@@ -3,4 +3,11 @@ public class ASTNil implements ASTNode {
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         return new VList();
     }
+    
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefs) throws TypeError {
+        // nil can be a list of any type - we need type inference here
+        // For now, return a polymorphic list type or require explicit typing
+        throw new TypeError("nil requires explicit type annotation");
+    }
 }
