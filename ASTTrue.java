05cb4c5b7e2d0a470ad1b7e3e005f591be382bbb
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 22:41:50 +0100

    feat(ASTTrue): add typecheck method for ASTTrue class

--- ASTTrue.java
+++ ASTTrue.java
@@ -3,4 +3,9 @@ public class ASTTrue implements ASTNode {
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         return new VBool(true);
     }
+    
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefs) throws TypeError {
+        return new ASTTBool();
+    }
 }
