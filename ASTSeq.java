aa241827e6574c4195bd686617e6b83b18579173
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 22:58:55 +0100

    feat(ASTSeq): implement typecheck method for ASTSeq class

--- ASTSeq.java
+++ ASTSeq.java
@@ -10,7 +10,15 @@ public class ASTSeq implements ASTNode {
     @Override
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         this.first.eval(e);
-
         return this.second.eval(e);
+    }
+    
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefs) throws TypeError {
+        // Type check first expression but ignore its type
+        this.first.typecheck(gamma, typeDefs);
+        
+        // Return type of second expression
+        return this.second.typecheck(gamma, typeDefs);
     }
 }
