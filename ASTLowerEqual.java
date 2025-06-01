0a062dd158a9ebc7ea20b373ffef6623dd6efa90
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 23:04:46 +0100

    feat(ASTComparison): implement typecheck methods for comparison operators

--- ASTLowerEqual.java
+++ ASTLowerEqual.java
@@ -17,4 +17,19 @@ public class ASTLowerEqual implements ASTNode {
             
         return new VBool(((VInt)v1).getVal() <= ((VInt)v2).getVal());
     }
+
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefs) throws TypeError {
+        ASTType leftType = this.lhs.typecheck(gamma, typeDefs);
+        ASTType rightType = this.rhs.typecheck(gamma, typeDefs);
+        
+        if (!(leftType instanceof ASTTInt) && leftType != null) {
+            throw new TypeError("Left operand of <= must be int, got " + leftType.toStr());
+        }
+        if (!(rightType instanceof ASTTInt) && rightType != null) {
+            throw new TypeError("Right operand of <= must be int, got " + rightType.toStr());
+        }
+        
+        return new ASTTBool();
+    }
 }
