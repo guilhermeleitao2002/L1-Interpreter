c7df2d5a8a68d38b0a191d58873e10baa452e934
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-01 22:42:33 +0100

    feat(ASTFalse): implement typecheck method for ASTFalse class

--- ASTFalse.java
+++ ASTFalse.java
@@ -3,4 +3,9 @@ public class ASTFalse implements ASTNode {
     public IValue eval(Environment<IValue> e) throws InterpreterError {
         return new VBool(false);
     }
+    
+    @Override
+    public ASTType typecheck(TypeEnvironment gamma, TypeDefEnvironment typeDefs) throws TypeError {
+        return new ASTTBool();
+    }
 }
