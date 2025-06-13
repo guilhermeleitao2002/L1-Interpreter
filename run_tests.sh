352bd4459a98f41747b97ce875da93e9e2ae9857
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2025-06-13 19:03:36 +0100

    refactor(tests): update typechecking tests to include MoreCircle type and expected outputs

--- run_tests.sh
+++ run_tests.sh
@@ -80,7 +80,7 @@ for test_num in {1..8}; do
 done
 
 # Do the same for static typechecking tests
-for test_num in {1..13}; do
+for test_num in {1..14}; do
     test_file="tests/typecheck/test${test_num}.l0"
     expected_file="tests/typecheck/output/test${test_num}.out"
 
